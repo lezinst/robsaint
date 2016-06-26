3e841c4836cf6413d8afe294033604a8f1bca364
Rob <robstlezin@gmail.com>
2016-06-25 21:27:05 -0600

    added index file and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var App = angular.module('myWebsite', ['ngMaterial, ng-Aria,']);
+
+// app.controller('appCtrl', function($scope){
+// 	$scope.contact = {
+// 	    showTooltip : true,
+// 	    tipDirection : ''
+// 	  };
+// 	 $scope.contact.showTooltip = function(val){
+
+// 	 } 
+};
+
+
+
