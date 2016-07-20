9b53e7fe1b3a0d88b8719c7a19e96da9947b5cc5
Rob <robstlezin@gmail.com>
2016-07-20 01:16:11 -0600

    edited for better responsiveness

--- app.js
+++ app.js
@@ -42,27 +42,27 @@ App.controller('control', function($scope,$location, $anchorScroll){
                 }
              ];
 
-            $scope.arrayData = [
+            // $scope.arrayData = [
                
-                {
-                    intro:"Rob St. Lezin",
-                    paragraph:" Hello, I'm a web developer. A University of Utah alumni since 2011, I graduated from DevMountain code school in Salt Lake City with an emphasis on AngularJS, NodeJS, and MongoDB. My mission is to write better code every day, to help build a better tomorrow. In my free time I like to stay active with my side-kick aussies,  make some craft brew with my neighbor, ride my bike around town, or stay involved with the development community.",
-                    callsign:"",
-                    callsigntwo:"../img/manager.svg"
+            //     {
+            //         intro:"Rob St. Lezin",
+            //         paragraph:" Hello, I'm a web developer. A University of Utah alumni since 2011, I graduated from DevMountain code school in Salt Lake City with an emphasis on AngularJS, NodeJS, and MongoDB. My mission is to write better code every day, to help build a better tomorrow. In my free time I like to stay active with my side-kick aussies,  make some craft brew with my neighbor, ride my bike around town, or stay involved with the development community.",
+            //         callsign:"",
+            //         callsigntwo:"../img/manager.svg"
 
                     
-                },
-                {
-                    intro:"Contact ",
-                    paragraph:"robstlezin@gmail.com | (801) 502-3196 | github.com/lezinst | linkedin.com/in/robertstlezin",
-                    callsigntwo:"../img/manager.svg"
+            //     },
+            //     {
+            //         intro:"Contact ",
+            //         paragraph:"robstlezin@gmail.com | (801) 502-3196 | github.com/lezinst | linkedin.com/in/robertstlezin",
+            //         callsigntwo:"../img/manager.svg"
 
                     
-                }
+            //     }
 
                 
                  
-            ];
+            // ];
 
 });
 
